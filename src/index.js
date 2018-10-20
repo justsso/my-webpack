4d0d232b4279aeb1beb1f241dd3932e2c203646a
sso just <528226249@qq.com>
2018-10-20 17:45:31 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 var _ = require('lodash');
-
+import './index.css';
+import HaiXing from './haixing.jpg';
+import Data from './my.xml';
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    var myImage = new Image();
+    myImage.src = HaiXing;
+    element.appendChild(myImage);
+    console.log(Data);
     return element;
 }
 
