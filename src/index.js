cfedef774eaa4efa1ca0073841a47293ece40337
sso just <528226249@qq.com>
2018-10-20 17:03:22 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var _ = require('lodash');
+
+function component() {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+}
+
+document.body.appendChild(component());
